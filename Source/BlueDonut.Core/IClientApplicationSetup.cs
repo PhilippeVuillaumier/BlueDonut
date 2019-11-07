b10c069e257d84a44733650f5d08341cd00ab3a8
PhilippeV <45167480+PhilippeVuillaumier@users.noreply.github.com>
2019-11-07 14:29:10 +0100

    The base starting point for the BlueDonut App
    DNA + NewtonSoft

---
+++ Source/BlueDonut.Core/IClientApplicationSetup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// The core functionality shared between all parts of the BlueDonut application
+/// </summary>
+namespace BlueDonut.Core
+{
+    /// <summary>
+    /// The entry point into the BlueDonut application used to setup the environment
+    /// All top-level application should create an instance of this
+    /// </summary>
+    public interface IClientApplicationSetup
+    {
+
+
+
+    }
+}
